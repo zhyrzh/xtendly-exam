20e2da9162f7db77277670c8bd63bf4c04068907
unknown <arongrhyz@gmail.com>
2022-07-25 13:44:31 +0800

    implemented button form

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,29 @@ function App() {
           <span className="home__small-font">Way to Close Your Books!</span>
         </h1>
       </div>
-      <div className="form"></div>
+      {/* Form */}
+      <div className="home__form">
+        <div className="home__form-left-title">
+          <h1 className="home__form-left-title-primary">Schedule A Demo</h1>
+          <h3 className="home__form-left-title-secondary">
+            Learn More About FloQast
+          </h3>
+        </div>
+        <div className="home__form-content">
+          <h1 className="home__form-content-title">
+            Learn How FloQast Can Improve Your Month-End
+          </h1>
+          <div className="home__form-inputs">
+            <input
+              className="home__input"
+              type="text"
+              placeholder="First Name"
+            />
+            <input className="home__input" type="text" placeholder="Email" />
+            <button className="home__input-submit">Schedule Now</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
